d3dcef6e9047130784daa50da95b56d47da18da7
Raka M.A <rakamuhammadalhafidz12312@gmail.com>
2022-03-10 16:07:10 +0700

    update list

---
+++ bwa_dart_training/list.dart
@@ -0,0 +1,8 @@
+//List berfungsi untuk menyimpan sebuah data atau object.
+//Pembuatan List harus diawali dengan kurung siku [].
+//List dimulai dari index 0
+void main() {
+  List mahasiswa = ['Rifqi', 'Hanif', 'Faqih'];    //Kalau langsung diprint, maka hasilnya jadi: [Rifqi, Hanif, Faqih]
+
+  print(mahasiswa);
+}
