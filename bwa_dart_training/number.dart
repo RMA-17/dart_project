1f32291d676e005156c48959e57ed0e4ea07e712
Raka M.A <rakamuhammadalhafidz12312@gmail.com>
2022-03-10 16:02:02 +0700

    finish numbering

--- bwa_dart_training/number.dart
+++ bwa_dart_training/number.dart
@@ -1,8 +1,25 @@
 void main() {
   num angka = 20;
+  int angka1 = 20;
+  double angka2 = 20.7;
 
   // typedata Number bisa dituliskan dengan 'num'
-  
+  // num = penggabungan dari int dan double (dia akan berubah tergantung value nya)
 
-  print(angka);
+  print(angka.runtimeType);
+  print(angka1.runtimeType);  //  variable.runtimeType = mengecek typedata nya.
+  print(angka2.toString().runtimeType); //  .toString() = menjadikan String
+
+  print(angka2.floor());  //pembulatan kebawah:
+
+  print(angka2.ceil());   //pembulatan keatas
+
+  print(angka2.round());  //pembulatan terdekat
+
+  print(angka2.toStringAsFixed(2)); // int.toStringAsFixed(berapa digit dibelakang koma) =  Untuk mengambil desimal dan berapa digit dibelakang koma nya.
+
+  print(angka2.toStringAsPrecision(3)); // int.toStringAsPrecision(index sampe berapa) sama kek indexing huruf. Tapi ini integer.
+
+
+  //Sisanya explore sendiri
 }
