ff8ba878fc3b924dcbf9cfb999160bc2879e8889
Raka M.A <rakamuhammadalhafidz12312@gmail.com>
2022-03-11 15:59:19 +0700

    add map.dart

--- bwa_dart_training/list.dart
+++ bwa_dart_training/list.dart
@@ -5,4 +5,27 @@ void main() {
   List mahasiswa = ['Rifqi', 'Hanif', 'Faqih'];    //Kalau langsung diprint, maka hasilnya jadi: [Rifqi, Hanif, Faqih]
 
   print(mahasiswa);
+
+  //Kalau di kotlin: mahasiswa.get(index)
+  //Kalau di dart: mahasiswa.elementAt(index)
+
+  mahasiswa.length; //Menampilkan ukuran List (di Kotlin: mahasiswa.size())
+
+  // mahasiswa.add('nama_orang');  //menambahkan value kedalam string
+
+  //Menambahkan list kedalam list:
+  var mahasiswa2 = ['Mamang', 'Babang', 'Sumamang'];
+  mahasiswa.addAll(mahasiswa2);
+  print(mahasiswa);
+
+  // Mengurutkan list: (menggunakan .sort())
+  mahasiswa.sort();
+
+  //Membalik list:
+  var mahasiswaReverse = mahasiswa.reversed.toList();
+  print(mahasiswaReverse);
+
+  //menghapus list:
+  mahasiswa.clear();
+
 }
---
+++ bwa_dart_training/map.dart
@@ -0,0 +1,15 @@
+void main() {
+
+  //Membuat Map:    [Map mahasiswa = {}]
+  var mahasiswa = {
+    'nama':'Raka M.A',
+    'umur': 16,
+    'nim':10929123123
+//  key:value
+  };
+
+  //Map sama saja seperti List, tapi Map memiliki key value, seperti list JSON.
+  //Sama seperti list, jika kita tidak menuliskan typedatanya, (Map<T>) dia akan menjadi dynamic
+
+  print(mahasiswa);
+}
